e96f4a524df6aba71b8ae57ff88f7b48e41150ad
milesrichmond <miles.richmond24@d155.org>
2023-11-15 09:29:19 -0600

    add assignment action

--- homework_tracker/AddViewController.swift
+++ homework_tracker/AddViewController.swift
@@ -15,19 +15,20 @@ class AddViewController: UIViewController {
     @IBOutlet weak var date_input: UIDatePicker!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func add_action(_ sender: UIButton) {
+        let name = name_textField.text
+        let description = description_textField.text
+        let class = class_textField.text
+        let date = date_input.date
+        
+        let newAssignment = Assignment(dueDate: date, assignmentName: name, fromClass: class, description: description)
+        
+        AppData.assignments.append(newAssignment)
+        
+        name_textField.text = ""
+        description_textField.text = ""
+        class_textField.text = ""
     }
-    */
-
 }
