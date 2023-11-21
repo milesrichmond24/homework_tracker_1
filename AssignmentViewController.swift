6175dfede76398e0875affc2b40e7b1d5d7b3c79
milesrichmond <miles.richmond24@d155.org>
2023-11-21 09:42:42 -0600

    Assignment view

---
+++ AssignmentViewController.swift
@@ -0,0 +1,33 @@
+//
+//  AssignmentViewController.swift
+//  homework_tracker
+//
+//  Created by MILES RICHMOND on 11/21/23.
+//
+
+import UIKit
+
+class AssignmentViewController: UIViewController {
+
+    @IBOutlet weak var date_label: UILabel!
+    @IBOutlet weak var desc_label: UILabel!
+    @IBOutlet weak var class_label: UILabel!
+    @IBOutlet weak var name_label: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
